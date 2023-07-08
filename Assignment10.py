ac8b0c1d5ae3ceeb9f5ea9a648e267b7c4a8ffcc
Aman Swastik Rath <72224485+Aman123-stack@users.noreply.github.com>
2023-07-09 01:14:26 +0530

    Add files via upload

--- Assignment10.py
+++ Assignment10.py
@@ -1,25 +1,94 @@
-q1>class Solution:
-    def arrayPairSum(self, nums: List[int]) -> int:
-        nums.sort()
-        sum1=0
-        for i in range(0,len(nums),2):
-            sum1=sum1+nums[i]
-        return sum1
+q1>def isPowerOfThree(n):
+    while n > 1:
+        if n % 3 != 0:
+            return False
+        n /= 3
+    return n == 1
 
-q2>class Solution:
-    def distributeCandies(self, candyType: List[int]) -> int:
-        set1=set()
-        n=len(candyType)
-        for i in range(len(candyType)):
-            set1.add(candyType[i])
-        m=len(set1)
-        return int(min(n/2,m))
+q2>def lastRemaining(n):
+    left_to_right = True
+    remaining = n
+    step = 1
+    head = 1
 
-q3>class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-        total=0
-        for i in range(len(flowerbed)-1):
-            if(flowerbed[i-1]==0 and flowerbed[i]==0 and flowerbed[i+1]==0):
-                flowerbed[i]=1
-                n-=1
-        return n<=0
+    while remaining > 1:
+        if left_to_right or remaining % 2 == 1:
+            head += step
+
+        remaining //= 2
+        step *= 2
+        left_to_right = not left_to_right
+
+    return head
+q3>def printSubsets(s, curr="", index=0):
+    if index == len(s):
+        print(curr)
+        return
+
+    # Exclude current element
+    printSubsets(s, curr, index + 1)
+
+    # Include current element
+    printSubsets(s, curr + s[index], index + 1)
+
+# Test the function
+printSubsets("abc")
+
+q4>def calculateLength(s):
+    if s == "":
+        return 0
+    else:
+        return 1 + calculateLength(s[1:])
+
+# Test the function
+print(calculateLength("Hello, World!"))  # Output: 13
+
+q5>def countSubstrings(s):
+    count = 0
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            if s[i] == s[j]:
+                count += 1
+    return count
+
+# Test the function
+print(countSubstrings("abc"))  # Output: 3
+q6>def towerOfHanoi(n, source, target, auxiliary):
+    if n == 1:
+        print("Move disk 1 from rod", source, "to rod", target)
+        return 1
+
+    moves = 0
+    moves += towerOfHanoi(n - 1, source, auxiliary, target)  # Move n-1 disks from source to auxiliary
+    print("Move disk", n, "from rod", source, "to rod", target)
+    moves += 1
+    moves += towerOfHanoi(n - 1, auxiliary, target, source)  # Move n-1 disks from auxiliary to target
+
+    return moves
+
+# Test the function
+n = 3
+total_moves = towerOfHanoi(n, 1, 3, 2)
+print("Total moves:", total_moves)
+q7>def permute(s, left, right):
+    if left == right:
+        print("".join(s))
+    else:
+        for i in range(left, right + 1):
+            s[left], s[i] = s[i], s[left]  # Swap characters
+            permute(s, left + 1, right)  # Recursively find permutations of remaining characters
+            s[left], s[i] = s[i], s[left]  # Backtrack (restore the original order)
+
+# Test the function
+s = "abc"
+permute(list(s), 0, len(s) - 1)
+q8>def countConsonants(s):
+    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+    count = 0
+    for char in s:
+        if char in consonants:
+            count += 1
+    return count
+
+# Test the function
+print(countConsonants("Hello, World!"))  # Output: 7
